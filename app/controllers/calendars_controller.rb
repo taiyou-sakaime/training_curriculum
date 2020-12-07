5cc33c7dc0a9e5682085701c9561457ea49cad89
taiyou-sakaime <taiyou.1120@docomo.ne.jp>
2020-12-07 17:17:35 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,13 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      days = {month: (@todays_date + x).month, date: @todays_date.day + x, plans: today_plans, wday: @week_days}
+      wday_num = @todays_date.wday
+      if wday_num <= 7
+        wday_num = wday_num -7
+      end
+
+
+      days = {month: (@todays_date + x).month, date: @todays_date.day + x, plans: today_plans, wday: wdays[wday_num] }
       @week_days.push(days)
     end
 
